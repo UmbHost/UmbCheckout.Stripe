beb29247380a80a7d29585fe9243a9d79fee10e0
Aaron <aaron@umbhost.net>
2023-08-14 14:34:23 +0100

    Moved and renamed Basket view component

---
+++ ViewComponents/StripeBasketViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UmbCheckout.Core.Interfaces;
+using UmbCheckout.Core.ViewModels;
+using UmbCheckout.Stripe.ViewModels;
+
+namespace UmbCheckout.Stripe.ViewComponents
+{
+    /// <summary>
+    /// View component for the Stripe basket
+    /// </summary>
+    [ViewComponent(Name = "StripeBasket")]
+    public class StripeBasketViewComponent : ViewComponent
+    {
+        private readonly IBasketService _basketService;
+
+        public StripeBasketViewComponent(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string? tableCssClass = null)
+        {
+            var basket = await _basketService.Get();
+
+            var model = new StripeBasketViewModel
+            {
+                TableCssClass = tableCssClass,
+                Basket = basket,
+                SubTotal = await _basketService.SubTotal(),
+                TotalItems = await _basketService.TotalItems()
+            };
+
+            return View("~/Views/Partials/UmbCheckout/_StripeBasket.cshtml", model);
+        }
+    }
+}
---
+++ ViewModels/BasketViewModel.cs
@@ -0,0 +1,17 @@
+﻿using UmbCheckout.Shared.Models;
+
+namespace UmbCheckout.Stripe.ViewModels
+{
+    /// <summary>
+    /// View model for the Basket View Component
+    /// </summary>
+    public class StripeBasketViewModel
+    {
+        public string? TableCssClass { get; set; } = null;
+
+        public Basket Basket { get; set; } = new Basket();
+
+        public decimal SubTotal { get; set; }
+        public long TotalItems { get; set; }
+    }
+}
