1c60cef7c724798fef656fddd8c236613e58abb1
Aaron <aaron@umbhost.net>
2023-08-14 15:01:50 +0100

    Added more properties

--- ViewComponents/StripeBasketViewComponent.cs
+++ ViewComponents/StripeBasketViewComponent.cs
@@ -18,13 +18,50 @@ namespace UmbCheckout.Stripe.ViewComponents
             _basketService = basketService;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(string? tableCssClass = null)
+        public async Task<IViewComponentResult> InvokeAsync(
+            string? productNameColumnText = "Name", 
+            string? productPriceColumnText = "Price", 
+            string? quantityColumnText = "Quantity", 
+            string? reduceColumnText = "Reduce", 
+            string? increaseColumnText = "Increase",
+            string? removeColumnText = "Remove", 
+            string? tableCssClass = null,
+            string? reduceButtonCssClass = null,
+            string? reduceButtonText = "-",
+            string? increaseButtonCssClass = null,
+            string? increaseButtonText = "+",
+            string? removeButtonCssClass = null,
+            string? removeButtonText = "Remove",
+            string? emptyBasketText  = "Your basket is empty!",
+            string? subTotalText = "Sub Total:",
+            string? formatCurrency = "GBP",
+            string? subTotalInformationText = "Coupons, Shipping and Tax are calculated on the next checkout step",
+            string? checkoutButtonCssClass = null, 
+            string checkoutButtonText = "Checkout")
         {
             var basket = await _basketService.Get();
 
             var model = new StripeBasketViewModel
             {
                 TableCssClass = tableCssClass,
+                CheckoutButtonCssClass = checkoutButtonCssClass,
+                CheckoutButtonText = checkoutButtonText,
+                ProductNameColumnText = productNameColumnText,
+                ProductPriceColumnText = productPriceColumnText,
+                QuantityColumnText = quantityColumnText,
+                RemoveColumnText = removeColumnText,
+                ReduceColumnText = reduceColumnText,
+                IncreaseColumnText = increaseColumnText,
+                ReduceButtonCssClass = reduceButtonCssClass,
+                ReduceButtonText = reduceButtonText,
+                IncreaseButtonCssClass = increaseButtonCssClass,
+                IncreaseButtonText = increaseButtonText,
+                RemoveButtonCssClass = removeButtonCssClass,
+                RemoveButtonText = removeButtonText,
+                EmptyBasketText = emptyBasketText,
+                SubTotalText = subTotalText,
+                FormatCurrency = formatCurrency,
+                SubtotalInformationText = subTotalInformationText,
                 Basket = basket,
                 SubTotal = await _basketService.SubTotal(),
                 TotalItems = await _basketService.TotalItems()
