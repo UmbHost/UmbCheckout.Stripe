56ab1d95257ec9cce18526734eff540c4df4e249
Aaron <aaron@umbhost.net>
2023-08-23 18:05:36 +0100

    Added "Add To Basket Button" View Component

---
+++ ViewComponents/StripeAddToBasketButtonViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UmbCheckout.Stripe.ViewModels;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace UmbCheckout.Stripe.ViewComponents
+{
+    /// <summary>
+    /// A view component for the Basket link with total item count
+    /// </summary>
+    [ViewComponent(Name = "StripeAddToBasketButton")]
+    public class StripeAddToBasketButtonViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(IPublishedContent product, string linkName = "Add to Basket", string? linkCssClass = null, Guid? returnGuid = null)
+        {
+            var model = new StripeAddToBasketButtonViewModel
+            {
+                Product = product,
+                LinkCssClass = linkCssClass,
+                LinkName = linkName,
+                ReturnGuid = returnGuid
+            };
+
+            return View("~/Views/Partials/UmbCheckout/_StripeAddToBasketButton.cshtml", model);
+        }
+    }
+}
---
+++ ViewModels/StripeAddToBasketButtonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace UmbCheckout.Stripe.ViewModels
+{
+    /// <summary>
+    /// View model for the Add to Basket Button View Component
+    /// </summary>
+    public class StripeAddToBasketButtonViewModel
+    {
+
+        public string? LinkCssClass { get; set; } = null;
+
+        public string LinkName { get; set; } = string.Empty;
+
+        public Guid? ReturnGuid { get; set; } = null;
+        public IPublishedContent? Product { get; set; } = null;
+    }
+}
