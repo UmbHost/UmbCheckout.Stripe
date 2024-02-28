e8363af90e6783765dc7f09e5fbd3695833a0cf5
Aaron Sadler <aaron@umbhost.net>
2024-02-28 21:14:56 +0000

    Added ShippingRate webhooks

--- Composers/RegisterServicesComposer.cs
+++ Composers/RegisterServicesComposer.cs
@@ -4,6 +4,10 @@ using UmbCheckout.Stripe.Services;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 
+#if NET8_0_OR_GREATER
+using UmbCheckout.Stripe.Webhooks;
+#endif
+
 namespace UmbCheckout.Stripe.Composers
 {
     public class RegisterServicesComposer : IComposer
@@ -14,6 +18,11 @@ namespace UmbCheckout.Stripe.Composers
             builder.Services.AddTransient<IStripeShippingRateDatabaseService, StripeShippingRateDatabaseService>();
             builder.Services.AddTransient<IStripeShippingRateApiService, StripeShippingRateApiService>();
             builder.Services.AddTransient<IStripeSettingsService, StripeSettingsService>();
+
+#if NET8_0_OR_GREATER
+            builder.WebhookEvents().Add<OnShippingRateDeletedWebhook>();
+            builder.WebhookEvents().Add<OnShippingRateSavedWebhook>();
+#endif
         }
     }
 }
---
+++ Webhooks/OnShippingRateDeletedWebhook.cs
@@ -0,0 +1,24 @@
+﻿#if NET8_0_OR_GREATER
+using Microsoft.Extensions.Options;
+using UmbCheckout.Stripe.Notifications;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Sync;
+using Umbraco.Cms.Core.Webhooks;
+
+namespace UmbCheckout.Stripe.Webhooks
+{
+    [WebhookEvent("Shipping Rate Deleted")]
+    public class OnShippingRateDeletedWebhook(
+        IWebhookFiringService webhookFiringService,
+        IWebhookService webhookService,
+        IOptionsMonitor<WebhookSettings> webhookSettings,
+        IServerRoleAccessor serverRoleAccessor)
+        : WebhookEventBase<OnShippingRateDeletedNotification>(webhookFiringService, webhookService, webhookSettings,
+            serverRoleAccessor)
+    {
+        public override string Alias => "OnShippingRateDeletedWebhook";
+    }
+}
+
+#endif
