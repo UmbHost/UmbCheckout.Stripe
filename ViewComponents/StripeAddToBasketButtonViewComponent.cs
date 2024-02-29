cfca2bb57fd8835e1eb838edda178ac66eccdbc1
Aaron Sadler <aaron@umbhost.net>
2024-02-29 20:36:11 +0000

    Disabled compiler warning

--- ViewComponents/StripeAddToBasketButtonViewComponent.cs
+++ ViewComponents/StripeAddToBasketButtonViewComponent.cs
@@ -10,7 +10,9 @@ namespace UmbCheckout.Stripe.ViewComponents
     [ViewComponent(Name = "StripeAddToBasketButton")]
     public class StripeAddToBasketButtonViewComponent : ViewComponent
     {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task<IViewComponentResult> InvokeAsync(IPublishedContent product, string? quantityLabel = "Quantity", bool showQuantity = true, string? inputCssClass = null, string? selectCssClass = null, string? labelCssClass = null, string? formGroupSpacerClass = null, string? variantSelectLabel = null, string buttonText = "Add to Basket", string? buttonCssClass = null, Guid? returnGuid = null, string? productNameAlias = null, string? currencyCode = null)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             var model = new StripeAddToBasketButtonViewModel
             {
